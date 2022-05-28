b0ac5ee0bbaa5bd17a561beacbdc897ebe632223
Harshil Surendralal <harshilsure+github@gmail.com>
2022-05-28 15:03:26 +0100

    Update default number of trials

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 INTERNET_RESULTS_FILE = Path("internet-test-results.csv")
 PING_RESULTS_FILE = Path("ping-test-results.csv")
-INTERNET_TRIALS = 25
-PING_TRIALS = 50
+INTERNET_TRIALS = 10
+PING_TRIALS = 10
